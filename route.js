bbf1ace1b3c8e5b6263a0b7da8c7944fa9c3c44d
FLINKgq <146103429+FLINKgq@users.noreply.github.com>
2024-04-25 18:07:42 +0300

    Update route.js

--- route.js
+++ route.js
@@ -1,20 +1,28 @@
+// Подключение Express.js для работы с маршрутами
 const express = require("express");
+// Создание маршрутизатора Express
 const router = express.Router();
 
+
 router.get("/", (req, res) => {
+  // Установка заголовков CORS для разрешения запросов из любого источника
   res.setHeader("Access-Control-Allow-Origin", "*");
 
+  // Установка разрешенных HTTP методов
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
+
 
   res.setHeader(
     "Access-Control-Allow-Headers",
     "X-Requested-With,content-type"
   );
 
+  // Отправка ответа с сообщением "Это только мой мир."
   res.send("Это только мой мир.");
 });
 
+// Экспорт маршрута для использования в других модулях
 module.exports = router;
