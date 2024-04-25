3bb15cac49bb87d7716c35221c159c124e5870c0
FLINKgq <146103429+FLINKgq@users.noreply.github.com>
2024-04-25 13:30:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
+// Подключение модулей
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
-const app = express();
-
 const route = require("./route");
 const { addUser, findUser, getRoomUsers, removeUser } = require("./users");
 
+
+// Использование CORS (Cross-Origin Resource Sharing) для обеспечения доступа к ресурсам с других доменов
 app.use(cors({ origin: "*" }));
 app.use(route);
 
+// Создание HTTP сервера на основе Express приложения
 const server = http.createServer(app);
 
+// Создание объекта Socket.IO и привязка его к HTTP серверу
 const io = new Server(server, {
   cors: {
     origin: "*",
@@ -19,40 +22,56 @@ const io = new Server(server, {
   },
 });
 
+// Обработка события подключения к серверу Socket.IO
 io.on("connection", (socket) => {
+  // Здесь обрабатываются все действия, связанные с подключением клиентов
+
+  // Обработка события "join" (присоединение пользователя к комнате)
   socket.on("join", ({ name, room }) => {
+    // Здесь пользователь присоединяется к комнате и отправляются сообщения
     socket.join(room);
 
+    // Добавление пользователя в список
     const { user, isExist } = addUser({ name, room });
 
+    // Формирование сообщения для нового пользователя
     const userMessage = isExist
       ? `${user.name}, here you go again`
       : `Hey my love ${user.name}`;
 
+    // Отправка сообщения новому пользователю
     socket.emit("message", {
       data: { user: { name: "Admin" }, message: userMessage },
     });
 
+    // Отправка сообщения о присоединении пользователя к остальным пользователям в комнате
     socket.broadcast.to(user.room).emit("message", {
       data: { user: { name: "Admin" }, message: `${user.name} has joined` },
     });
 
+    // Отправка обновленного списка пользователей в комнате
     io.to(user.room).emit("room", {
       data: { users: getRoomUsers(user.room) },
     });
   });
 
+  // Обработка события "sendMessage" (отправка сообщения в комнату)
   socket.on("sendMessage", ({ message, params }) => {
+    // Поиск пользователя по параметрам
     const user = findUser(params);
 
+    // Если пользователь найден, отправляем сообщение в его комнату
     if (user) {
       io.to(user.room).emit("message", { data: { user, message } });
     }
   });
 
+  // Обработка события "leftRoom" (покидание комнаты)
   socket.on("leftRoom", ({ params }) => {
+    // Удаление пользователя из комнаты
     const user = removeUser(params);
 
+    // Если пользователь удален, отправляем сообщение о его выходе и обновляем список пользователей
     if (user) {
       const { room, name } = user;
 
@@ -66,11 +85,13 @@ io.on("connection", (socket) => {
     }
   });
 
+  // Обработка события "disconnect" (отключение клиента от сервера)
   io.on("disconnect", () => {
     console.log("Disconnect");
   });
 });
 
+// Запуск сервера на порту 5000
 server.listen(5000, () => {
   console.log("Server is running");
 });
