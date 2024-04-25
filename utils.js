5412284e9991d1b22895b3a790421998a3f87334
FLINKgq <146103429+FLINKgq@users.noreply.github.com>
2024-04-25 13:51:56 +0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,3 +1,5 @@
+// Функция trimStr обрезает лишние пробелы и приводит строку к нижнему регистру
 const trimStr = (str) => str.trim().toLowerCase();
 
+// Экспорт функции trimStr чтобы потом в других файлах импортировать ее и использовать
 exports.trimStr = trimStr;
